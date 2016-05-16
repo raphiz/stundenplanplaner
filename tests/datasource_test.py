2c83b23d8bb962c2892f26967c6c823473f4e0f5
Raphael Zimmermann <mail@raphael.li>
2016-05-16 13:14:45 +0200

    Record all tests

--- tests/datasource_test.py
+++ tests/datasource_test.py
@@ -1,9 +1,12 @@
 from planner import AdUnisHSR, AuthenticationException, utils
 import pytest
 from configparser import ParsingError
-import vcr
+from vcr import VCR
+
+vcr = VCR(cassette_library_dir='fixtures/')
 
 
+@vcr.use_cassette
 def test_signin_invalid_credentials():
     source = AdUnisHSR()
 
@@ -13,14 +16,21 @@ def test_signin_invalid_credentials():
     assert str(exceptionInfo.value) == "Authentication has failed (Status code was 401)!"
 
 
+@vcr.use_cassette
+def test_in_hsr_network():
+    source = AdUnisHSR()
+    assert source._in_HSR_network()
+
+
+@vcr.use_cassette
 def test_signin_valid_credentials():
-    with vcr.use_cassette('fixtures/test_signin_valid_credentials.yaml'):
-        source = AdUnisHSR()
-        username, password = utils.parse_user_credentials('auth.cfg')
-        response = source.signin(username, password)
-        assert len(response.text) > 100
+    source = AdUnisHSR()
+    username, password = utils.parse_user_credentials('auth.cfg')
+    response = source.signin(username, password)
+    assert len(response.text) > 100
 
 
+@vcr.use_cassette
 def test_all_modules_ids_not_signed_in():
     source = AdUnisHSR()
 
@@ -30,23 +40,23 @@ def test_all_modules_ids_not_signed_in():
     assert str(exceptionInfo.value) == "You must log in before you can query!"
 
 
+@vcr.use_cassette
 def test_all_modules_ids_signed_in_happy_path():
     source = AdUnisHSR()
 
-    with vcr.use_cassette('fixtures/test_all_modules_ids_signed_in_happy_path.yaml'):
-        username, password = utils.parse_user_credentials('auth.cfg')
-        response = source.signin(username, password)
-        modules = source.all_modules_ids()
-        assert len(modules) > 20
-        assert modules['InfSys'] == '51677'
+    username, password = utils.parse_user_credentials('auth.cfg')
+    response = source.signin(username, password)
+    modules = source.all_modules_ids()
+    assert len(modules) > 20
+    assert modules['InfSys'] == '51677'
 
 
+@vcr.use_cassette
 def test_lectures_times_happy_path():
     source = AdUnisHSR()
 
-    with vcr.use_cassette('fixtures/test_lectures_times_happy_path.yaml'):
-        username, password = utils.parse_user_credentials('auth.cfg')
-        response = source.signin(username, password)
-        lectures_times = source.lectures_times(['AD1', 'InfSi1'])
-        print(lectures_times)
-        # TODO: verify!
+    username, password = utils.parse_user_credentials('auth.cfg')
+    response = source.signin(username, password)
+    lectures_times = source.lectures_times(['AD1', 'InfSi1'])
+    print(lectures_times)
+    # TODO: verify!
