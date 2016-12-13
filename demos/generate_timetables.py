e3cdeabce54f72cba3ae8e45de8f345062018229
Raphael Zimmermann <mail@raphael.li>
2016-12-13 09:34:20 +0100

    Update demo for next semester

--- demos/generate_timetables.py
+++ demos/generate_timetables.py
@@ -7,27 +7,28 @@ import vcr
 
 
 with vcr.use_cassette('fixtures/demo_timetables', record_mode='new_episodes'):
-    module_spec = {'MsTe': {'v': 2, 'u': 2},
-                   'AD2': {'v': 2, 'u': 2},
-                   'BuRe1': [
-                    {'abbrev': 'ReIng', 'v': 2},
-                    {'abbrev': 'BuPl', 'v': 1, 'u': 1},
-                   ],
-                   'CPl': {'v': 2, 'u': 2},
-                   'MGE': {'v': 2, 'u': 2},
-                   'SE1': {'v': 2, 'u': 2},
-                   'WED2': {'v': 2, 'u': 2},
-                   'PrFm': {'v': 2, 'u': 2}
+    module_spec = {'Dbs2': {'v': 2, 'u': 2},
+                   'BuRe2a': [
+                    {'abbrev': 'ReVertr', 'v': 2},
+                    {'abbrev': 'ManagSim', 'u': 2},
+                    ],
+                   'InfSi3': {'v': 2, 'u': 2},
+                   'ParProg': {'v': 2, 'u': 2},
+                   'PhAI': {'v': 3, 'u': 1},
+                   'SE2': {'v': 2, 'u': 2},
+                   'Vss': {'v': 2, 'u': 2},
+                   'WI1': [
+                        {'abbrev': 'ITBus', 'v': 2, 'u': 2}
+                    ],
                    }
-
     source = AdUnisHSR()
     username, password = utils.parse_user_credentials('auth.cfg')
     response = source.signin(username, password)
 
     filters = [
-                restrictions.InTimeRange(time(7, 0), time(18, 00)),
-                restrictions.MinChance(30),
-                restrictions.FreeTime(3, range(5), time(12), time(23)),
+                # restrictions.InTimeRange(time(7, 0), time(18, 00)),
+                # restrictions.MinChance(30),
+                # restrictions.FreeTime(3, range(5), time(12), time(23)),V
                 # restrictions.FreeTime(1, range(5), time(6), time(23))
                 ]
 
